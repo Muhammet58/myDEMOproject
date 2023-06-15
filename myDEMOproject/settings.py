c26c7d38a3210214ae26a1a3a04ff058156a17b3
muhammet <yildirimuhammed058@gamil.com>
2023-06-16 02:09:49 +0300

    new django project

--- myDEMOproject/settings.py
+++ myDEMOproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oo7$%%%wzct_55a)pm_!k)8@dhpa5olahgz4*=l8*s#txxf)91
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
